f5661677cc7beb692c05ba462ba0bc35ecc8b558
Omar Desogus <me@cedoor.dev>
2020-03-04 15:22:43 +0100

    [feat] create shell script to automate emr configurations

---
+++ scripts/start.sh
@@ -0,0 +1,24 @@
+echo "Download scripts"
+
+ssh -i "$1" "$2" << EOF
+rm -fr *
+wget -q https://github.com/cedoor/brexit-lang/archive/master.zip
+unzip -q master.zip
+pip-3.6 install --user -q -r brexit-lang-master/requirements.txt
+echo "export PYSPARK_PYTHON=python3" >> ~/.bash_profile
+mkdir data
+EOF
+
+echo "Move data and .env"
+
+scp -r -i "$1" "$3" "$2:/home/hadoop"
+scp -i "$1" "$4" "$2:/home/hadoop/brexit-lang-master"
+
+echo "Run analysis"
+
+ssh -i "$1" "$2" << EOF
+hdfs dfs -put ~/data/* /data
+python3 brexit-lang-master/scripts/analysis_1.py
+spark-submit brexit-lang-master/scripts/analysis_2.py
+spark-submit brexit-lang-master/scripts/analysis_3.py
+EOF
